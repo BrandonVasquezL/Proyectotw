d7a79a5d5493efd137cd096abbb2acf63dfc4829
unknown <zs20022115@estudiantes.uv.mx>
2023-04-11 17:31:50 -0600

    models

---
+++ Proyectotw/Proyectotw/Models/Alumno.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyectotw.Models
+{
+    public class Alumno : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyectotw/Proyectotw/Models/Materia.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyectotw.Models
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
