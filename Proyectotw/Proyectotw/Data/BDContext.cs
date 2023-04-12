3db097456a3a7f3362415b6fe2ddfef64287a7a8
unknown <zs20022115@estudiantes.uv.mx>
2023-04-11 21:36:39 -0600

    guid

---
+++ Proyectotw/Proyectotw/Data/BDContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyectotw.Data
+{
+    public class BDContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
