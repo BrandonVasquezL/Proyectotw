aa07f83ae5ee5cab5249086c598b5919920370bf
SantiagoECHM <111538733+SantiagoECHM@users.noreply.github.com>
2023-04-13 00:16:46 -0700

    materias

--- 2FrontTW/src/app/Componentes/materia/add-materia/materia.component.ts
+++ 2FrontTW/src/app/Componentes/materia/add-materia/materia.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Materias} from 'src/app/models/materias';
+import { MateriaService } from 'src/app/services/materia.service';
 
 @Component({
-  selector: 'app-materia',
+  selector: 'app-add-materia',
   templateUrl: './materia.component.html',
-  styleUrls: ['./materia.component.scss']
+  styleUrls: ['./materia.component.css']
 })
-export class MateriaComponent {
+export class MateriaComponent implements OnInit{
+  agregarMateriaRequest:Materias = {
+    id:"",
+    nrc: 0,
+    maestro:'',
+    cant_alumnos: 0
+  };
 
+  constructor(private materiasServicio: MateriaService, private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  agregarAlumno(){
+    this.materiasServicio.addMateria(this.agregarMateriaRequest)
+    .subscribe({
+      next:(materia)=>{
+        this.router.navigate(['materias']);
+      }
+    });
+  }
 }
--- 2FrontTW/src/app/services/materia.service.ts
+++ 2FrontTW/src/app/services/materia.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Materias } from '../models/materias';
+import { environment } from 'src/enviroments/enviroment.development';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MateriaService {
 
+export class MateriaService {
+  baseApiUrl:string = environment.baseApiUrl;
   constructor() { }
+  getAllMaterias():Observable<Materias[]>{
+    return this.http.get<Materias[]>(this.baseApiUrl+"/api/Materias");
+  }
+  addMateria(agregarMateriaRequest: Materias):Observable<Materias>{
+    agregarMateriaRequest.id='00000000-0000-0000-0000-000000000000';
+    return this.http.post<Materias>(this.baseApiUrl+"/api/Materias", agregarMateriaRequest);
+  }
+  editMateria(id:string, actualizarEstudianteRequest:Materias):Observable<Materias>{
+    return this.http.put<Materias>(this.baseApiUrl+"/api/Materias"+ id, actualizarEstudianteRequest );
+  }
+  deleteMateria(id:string):Observable<Materias>{
+    return this.http.delete<Materias>(this.baseApiUrl+"/api/Materias"+ id);
+  }
+  getMateria(id:string):Observable<Materias>{
+    return this.http.get<Materias>(this.baseApiUrl+"/api/Materias"+id);
+  } 
 }
